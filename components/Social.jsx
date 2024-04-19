e08ed928247dc8637f6971a6e8b13891b1ad9b2d
Sebastian Romero <s.romero@arduino.cc>
2024-04-19 15:30:23 +0200

    Add new social icons

--- components/Social.jsx
+++ components/Social.jsx
@@ -2,18 +2,12 @@ import React from "react";
 
 const SocialShare = [
   {
-    iconName: "facebook",
-    link: "https://www.facebook.com/",
+    iconName: "linkedin",
+    link: "https://www.linkedin.com/in/romeroseb/",
   },
-  { iconName: "twitter", link: "https://twitter.com/" },
   {
-    iconName: "instagram",
-    link: "https://www.instagram.com/",
-  },
-  { iconName: "dribbble", link: "https://dribbble.com/" },
-  {
-    iconName: "tik-tok",
-    link: "https://www.tiktok.com/",
+    iconName: "github",
+    link: "https://github.com/sebromero",
   },
 ];
 const Social = () => {
