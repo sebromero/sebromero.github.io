545f36a2dcb8c0bd8bebd04855528c7b7af3ec78
Sebastian Romero <s.romero@arduino.cc>
2024-05-02 12:58:19 +0200

    Add hyperlink to details card

--- components/cards/DetailsCard.jsx
+++ components/cards/DetailsCard.jsx
@@ -1,40 +1,44 @@
+import Link from "next/link";
+
 const DetailsCard = (props) => {
     return (
-        <div className="details-card" onClick={props.onClick}>
-            <div className="image">
-                <div
-                    className="main "
-                    style={{
-                    backgroundImage: `url(${props.image})`,
-                    }}
-                ></div>
-            </div>
-            {/* END IMAGE */}
-            <div className="details">
-                <h3 className="title">{props.title}</h3>
-                {/* END TITLE */}
-                
-                <div className="extra">
-                    <p className="date">
-                        {props.subtitle}
-                        <span>{props.date}</span>
-                    </p>
+        <Link href={props.href ? props.href : "" }>
+            <div className="details-card" onClick={props.onClick}>
+                <div className="image">
+                    <div
+                        className="main "
+                        style={{
+                        backgroundImage: `url(${props.image})`,
+                        }}
+                    ></div>
                 </div>
-                {/* END EXTRA */}
-
-
-                {/* START READ MORE BUTTON */} 
-                { props.showReadMore &&    
-                    <div className="tokyo_tm_read_more">
-                        <div className="read-more">
-                        <span>Read More</span>
-                        </div>
+                {/* END IMAGE */}
+                <div className="details">
+                    <h3 className="title">{props.title}</h3>
+                    {/* END TITLE */}
+                    
+                    <div className="extra">
+                        <p className="date">
+                            {props.subtitle}
+                            <span>{props.date}</span>
+                        </p>
                     </div>
-                }
-                {/* END READ MORE BUTTON */}
+                    {/* END EXTRA */}
+
+
+                    {/* START READ MORE BUTTON */} 
+                    { props.showReadMore &&    
+                        <div className="tokyo_tm_read_more">
+                            <div className="read-more">
+                            <span>Read More</span>
+                            </div>
+                        </div>
+                    }
+                    {/* END READ MORE BUTTON */}
+                </div>
+                {/* END DETAILS */}
             </div>
-            {/* END DETAILS */}
-        </div>
+        </Link>
     );
 }
 
