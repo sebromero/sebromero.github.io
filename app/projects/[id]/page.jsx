2143bbd08567bfc58f5059d8d344d91767d2333c
Sebastian Romero <s.romero@arduino.cc>
2024-06-16 15:12:30 +0200

    Rename slug

---
+++ app/projects/[id]/page.jsx
@@ -0,0 +1,54 @@
+// 'use client';
+
+// SEE: https://blog.kfirfitousi.com/posts/web-dev/mdx-nextjs-13
+
+import About from "@/components/about";
+import Sidebar from "@/components/sidebar/Sidebar";
+import { MDXContent } from "@/components/MDXContent";
+import { getAllProjects, getProjectsPath, getProject } from "@/lib/projects"
+
+async function Project({ id }) {
+  const { serialized, frontmatter } = await getProject(getProjectsPath() + id + '/project.mdx');
+  return (
+    <article>
+      <h1>{frontmatter?.title}</h1>
+      <p>{frontmatter?.date}</p>
+      <MDXContent source={serialized} />
+    </article>
+  )
+}
+
+// Disallowed in client mode
+// export const metadata = {
+//   title: "About || Tokyo - Personal Portfolio React Nextjs Template",
+// };
+const Page = ({ params }) => {
+  // const Project = dynamic(() => import("@/public/projects/" + params.project + "/project.mdx"));
+  return (
+    <>
+      {/* End page title for seo */}
+
+      <Sidebar />
+      {/* END LEFT MENU CONTENT */}
+
+      {/* START RIGHT PART CONTENT */}
+      <div className="rightpart">
+        <div className="rightpart_in">
+          <div className="tokyo_tm_section">
+            <div data-aos="fade-right" data-aos-duration="1200">            
+              {/* <About /> */}
+              <Project id={params.id} />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* END RIGHT PART CONTENT */}
+    </>
+  );
+};
+
+export async function generateStaticParams() {
+  return getAllProjects();
+}
+
+export default Page;
--- app/projects/[project]/layout.js
+++
@@ -1,4 +0,0 @@
-export default function MdxLayout({ children }) {
-    // Create any shared layout or styles here
-    return <div style={{ color: 'blue' }}>{children}</div>
-  }
