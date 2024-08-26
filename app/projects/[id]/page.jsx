7373d2bfb2a9e742749a80ccb525d1fa5d23d6ed
Sebastian Romero <s.romero@arduino.cc>
2024-08-26 13:32:03 +0200

    Remove unused code

--- app/projects/[id]/page.jsx
+++ app/projects/[id]/page.jsx
@@ -2,22 +2,9 @@
 
 // SEE: https://blog.kfirfitousi.com/posts/web-dev/mdx-nextjs-13
 
-import About from "@/components/about";
 import Sidebar from "@/components/sidebar/Sidebar";
-import { MDXContent } from "@/components/MDXContent";
 import { getAllProjects, getProjectsPath, getProject } from "@/lib/projects"
 import Project from "@/components/project";
-
-// async function Project({ id }) {
-//   const { title, date, content } = await getProject(id);
-//   return (
-//     <article>
-//       <h1>{title}</h1>
-//       <p>{date}</p>
-//       <MDXContent source={content} />
-//     </article>
-//   )
-// }
 
 // Disallowed in client mode
 // export const metadata = {
